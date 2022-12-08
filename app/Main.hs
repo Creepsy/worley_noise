a75f364df71cc4facaf5aa3ea903c90c85133ba3
Creepsy <Robin.gd@outlook.de>
2022-12-08 20:35:25 +0100

    basic image generation

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,12 @@
 module Main where
 
+import qualified Graphics.Image as Img
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    let img = Img.makeImage (100, 100) gradient :: Img.Image Img.VU Img.Y Double
+
+    Img.writeImage "heightmap.png" img
+
+gradient :: (Int, Int) -> Img.Pixel Img.Y Double
+gradient (xPos, yPos) = Img.PixelY $ fromIntegral (xPos * yPos) / 10000
