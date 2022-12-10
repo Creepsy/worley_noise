cfa8da2c6bfbd712076f02c1b7780ff45b6043fe
Creepsy <Robin.gd@outlook.de>
2022-12-10 19:37:56 +0100

    cleanup / refactoring

--- app/Main.hs
+++ app/Main.hs
@@ -20,8 +20,8 @@ main = do
         -- img = Img.makeImage (1000, 1000) (\pos -> Img.PixelY $ index2D (1000, 1000) pos heightmap) :: Img.Image Img.VU Img.Y Double
     -- let heightmap = WN.generateWorleyNoise (WN.WorldInfo 234234 10)
         -- img = Img.makeImage (2000, 2000) (\pos -> Img.PixelY $ index2D (2000, 2000) pos heightmap) :: Img.Image Img.VU Img.Y Double
-    let world = WN.WorldInfo 234234 100
-        img = Img.makeImage (2000, 2000) (\(x, y) -> Img.PixelY $ WN.worleyNoise world (fromIntegral x, fromIntegral y)) :: Img.Image Img.VU Img.Y Double
+    let world = WN.HeightMapInfo 234233 20
+        img = Img.makeImage (1000, 1000) (\(x, y) -> Img.PixelY $ WN.worleyNoise world (fromIntegral x, fromIntegral y)) :: Img.Image Img.VU Img.Y Double
 
     Img.writeImage "heightmap.png" img
 
--- app/WorleyNoise.hs
+++ app/WorleyNoise.hs
@@ -2,13 +2,11 @@ module WorleyNoise (
     Seed,
     Position,
     ChunkSize,
-    WorldInfo(..),
+    HeightMapInfo(..),
     worleyNoise
 ) where
 
-import GHC.Generics (Generic)
 import Data.Bits ((.&.), (.|.), Bits (shift, xor, shiftR, shiftL))
-import Debug.Trace (trace)
 import Data.Int (Int16)
 import qualified Data.Vector as Vec
 import Data.List (minimumBy)
@@ -18,38 +16,39 @@ type Seed = Int
 type Position = (Int16, Int16)
 type ChunkSize = Int16
 
-data WorldInfo = WorldInfo {
+data HeightMapInfo = HeightMapInfo {
     seed :: Seed,
     chunkSize :: ChunkSize
 }
 
 -- add implementation for generate many (with grid bounds as parameter)
 -- also add option to choose second or third closest point -> seperate for efficency ?
+-- maybe also remove casting between int 16 and int32; use int64?!?!
 
-worleyNoise :: WorldInfo -> Position -> Double
-worleyNoise worldInfo@(WorldInfo _ chunkSize) pos = fromIntegral distSquared / fromIntegral (chunkSize ^ 2) -- better way to calculate noise value?
-    where closest = closestFeaturePoint worldInfo pos
+worleyNoise :: HeightMapInfo -> Position -> Double
+worleyNoise hmInfo@(HeightMapInfo _ chunkSize) pos = fromIntegral distSquared / fromIntegral (chunkSize ^ 2) -- better way to calculate noise value?
+    where closest = closestFeaturePoint hmInfo pos
           distSquared = distanceSquared pos closest
 
-closestFeaturePoint :: WorldInfo -> Position -> Position
-closestFeaturePoint worldInfo pos = minimumBy (compare `on` distanceSquared pos) featurePoints
-    where featurePoints = map (generateFeaturePoint worldInfo) . surroundingChunks worldInfo $ pos
+closestFeaturePoint :: HeightMapInfo -> Position -> Position
+closestFeaturePoint hmInfo pos = minimumBy (compare `on` distanceSquared pos) featurePoints
+    where featurePoints = map (generateFeaturePoint hmInfo) . surroundingChunks hmInfo $ pos
 
-surroundingChunks :: WorldInfo -> Position -> [Position]
-surroundingChunks worldInfo pos = surroundings
-    where (currChunkX, currChunkY) = chunkPosition worldInfo pos
+surroundingChunks :: HeightMapInfo -> Position -> [Position]
+surroundingChunks hmInfo pos = surroundings
+    where (currChunkX, currChunkY) = chunkPosition hmInfo pos
           surroundings = [(currChunkX + xOff, currChunkY + yOff) | xOff <- [-1..1], yOff <- [-1..1]]
 
-chunkPosition :: WorldInfo -> Position -> Position
-chunkPosition (WorldInfo _ chunkSize) (xPos, yPos) = (xPos `div` chunkSize, yPos `div` chunkSize)
+chunkPosition :: HeightMapInfo -> Position -> Position
+chunkPosition (HeightMapInfo _ chunkSize) (xPos, yPos) = (xPos `div` chunkSize, yPos `div` chunkSize)
 
 distanceSquared :: Position -> Position -> Int
 distanceSquared (firstX, firstY) (secondX, secondY) = deltaX ^ 2 + deltaY ^ 2
     where deltaX = fromIntegral $ firstX - secondX
           deltaY = fromIntegral $  firstY - secondY
 
-generateFeaturePoint :: WorldInfo -> Position -> Position
-generateFeaturePoint (WorldInfo seed chunkSize) chunkPos@(chunkX, chunkY) = (xPos, yPos) 
+generateFeaturePoint :: HeightMapInfo -> Position -> Position
+generateFeaturePoint (HeightMapInfo seed chunkSize) chunkPos@(chunkX, chunkY) = (xPos, yPos) 
     where chunkHash = hash2D seed chunkPos
           deltaX = fromIntegral (chunkHash .&. 0xffff) `mod` chunkSize
           deltaY = fromIntegral ((chunkHash `shiftR` 16) .&. 0xffff) `mod` chunkSize
