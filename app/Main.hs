cdda87121bbb09c316eb7d68a11d64d7ed46c9f7
Creepsy <Robin.gd@outlook.de>
2022-12-11 11:34:47 +0100

    finished worley noise

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,7 @@ import Data.Vector ((!))
 import Data.List (sortBy)
 import Data.Function (on)
 import Debug.Trace (trace)
+import Data.List.Split (chunksOf)
 
 import qualified WorleyNoise as WN
 
@@ -16,51 +17,18 @@ type Seed = Int
 main :: IO ()
 main = do
 
-    -- let heightmap = generateWorleyNoise 634560789 (5, 5) 200
-        -- img = Img.makeImage (1000, 1000) (\pos -> Img.PixelY $ index2D (1000, 1000) pos heightmap) :: Img.Image Img.VU Img.Y Double
-    -- let heightmap = WN.generateWorleyNoise (WN.WorldInfo 234234 10)
-        -- img = Img.makeImage (2000, 2000) (\pos -> Img.PixelY $ index2D (2000, 2000) pos heightmap) :: Img.Image Img.VU Img.Y Double
-    let world = WN.HeightMapInfo 234233 20
-        img = Img.makeImage (1000, 1000) (\(x, y) -> Img.PixelY $ WN.worleyNoise world (fromIntegral x, fromIntegral y)) :: Img.Image Img.VU Img.Y Double
+    let octaves = [
+                (WN.HeightMapInfo 853456 100, 0.6),
+                (WN.HeightMapInfo 345345 50, 0.2),
+                (WN.HeightMapInfo 23442 40, 0.05),
+                (WN.HeightMapInfo 365346 25, 0.05),
+                (WN.HeightMapInfo 87823 10, 0.05),
+                (WN.HeightMapInfo 65436 5, 0.04),
+                (WN.HeightMapInfo 25345645 2, 0.01)]
+        img = Img.makeImage (1000, 1000) (Img.PixelY . octaveNoise octaves) :: Img.Image Img.VU Img.Y Double
+    
 
     Img.writeImage "heightmap.png" img
 
-index2D :: (Int, Int) -> Position -> Vec.Vector a -> a
-index2D (width, _) (xPos, yPos) toIndex = toIndex ! (yPos * width + xPos)
-
--- slow code, for reference only
-
--- generateWorleyNoise :: Seed -> (Int, Int) -> Int -> Vec.Vector Double
--- generateWorleyNoise seed size@(width, height) cellSize = Vec.generate (width * height * cellSize ^ 2) (generateValue borderedSize cellSize featurePoints . adaptedPosition)
---         where borderedSize = (width + 2, height + 2) -- neighbours for border cells
---               featurePoints = generateFeaturePoints seed borderedSize cellSize 
---               indexToPosition' = indexToPosition (width * cellSize, height * cellSize)
---               adaptedPosition = (\(xPos, yPos) -> (xPos + cellSize, yPos + cellSize)) . indexToPosition'
-
--- generateValue :: (Int, Int) -> Int -> Vec.Vector Position -> Position -> Double
--- generateValue size cellSize featurePoints pos@(posX, posY) = (fromIntegral . distanceSquared pos . head $ sortedNeighbours) / (0.25 * fromIntegral cellSize ^ 2)
---     where neighbours = neighbourFeaturePoints size cellSize (cellIndex cellSize pos) featurePoints
---           sortedNeighbours = sortBy (compare `on` distanceSquared pos) neighbours
-
--- distanceSquared :: Position -> Position -> Int
--- distanceSquared (firstX, firstY) (secondX, secondY) = deltaX ^ 2 + deltaY ^ 2 
---     where deltaX = firstX - secondX
---           deltaY = firstY - secondY
-
--- neighbourFeaturePoints :: (Int, Int) -> Int -> Position -> Vec.Vector Position -> [Position]
--- neighbourFeaturePoints size cellSize (cellX, cellY) featurePoints = zipWith asGlobalPosition neighbourCells . map (($ featurePoints) . index2D size) $ neighbourCells 
---     where neighbourCells = [(xPos, yPos) | xPos <- [cellX - 1 .. cellX + 1], yPos <- [cellY - 1 .. cellY + 1]]
---           asGlobalPosition (cellX, cellY) (posX, posY) = (cellX * cellSize + posX, cellY * cellSize + posY)
-
--- generateFeaturePoints :: Seed -> (Int, Int) -> Int -> Vec.Vector Position
--- generateFeaturePoints seed (width, height) cellSize = Vec.fromList $ take (width * height) randomPositions
---     where randomPositions = Rnd.randomRs ((0, 0), (cellSize - 1, cellSize - 1)) (Rnd.mkStdGen seed)
-
--- indexToPosition :: (Int, Int) -> Int -> Position
--- indexToPosition (width, height) index = (index `mod` width, index `div` width)
-
--- cellIndex :: Int -> Position -> Position
--- cellIndex cellSize (posX, posY) = (posX `div` cellSize, posY `div` cellSize) 
-
--- gradient :: (Int, Int) -> Img.Pixel Img.Y Double
--- gradient (xPos, yPos) = Img.PixelY $ fromIntegral (xPos * yPos) / 10000
+octaveNoise :: [(WN.HeightMapInfo, Double)] -> WN.Position -> Double
+octaveNoise octaves pos = foldl (\acc (oct, amplitude) -> acc  + amplitude * WN.worleyNoise oct pos) 0 octaves
--- app/WorleyNoise.hs
+++ app/WorleyNoise.hs
@@ -3,64 +3,76 @@ module WorleyNoise (
     Position,
     ChunkSize,
     HeightMapInfo(..),
-    worleyNoise
+    worleyNoise,
+    index2D
 ) where
 
-import Data.Bits ((.&.), (.|.), Bits (shift, xor, shiftR, shiftL))
-import Data.Int (Int16)
 import qualified Data.Vector as Vec
+
+import Data.Bits ((.&.), (.|.), Bits (shift, xor, shiftR, shiftL))
 import Data.List (minimumBy)
 import Data.Function (on)
+import Data.Vector ((!))
 
 type Seed = Int
-type Position = (Int16, Int16)
-type ChunkSize = Int16
+type Position = (Int, Int)
+type ChunkSize = Int
+type Size = (Int, Int)
 
 data HeightMapInfo = HeightMapInfo {
     seed :: Seed,
     chunkSize :: ChunkSize
 }
 
--- add implementation for generate many (with grid bounds as parameter)
--- also add option to choose second or third closest point -> seperate for efficency ?
--- maybe also remove casting between int 16 and int32; use int64?!?!
+-- add option to choose second or third closest point -> seperate for efficency ?
+
+-- normal worley noise
 
 worleyNoise :: HeightMapInfo -> Position -> Double
-worleyNoise hmInfo@(HeightMapInfo _ chunkSize) pos = fromIntegral distSquared / fromIntegral (chunkSize ^ 2) -- better way to calculate noise value?
+worleyNoise hmInfo@(HeightMapInfo _ chunkSize) pos = calculateNoiseValue hmInfo closest pos
     where closest = closestFeaturePoint hmInfo pos
-          distSquared = distanceSquared pos closest
 
 closestFeaturePoint :: HeightMapInfo -> Position -> Position
-closestFeaturePoint hmInfo pos = minimumBy (compare `on` distanceSquared pos) featurePoints
-    where featurePoints = map (generateFeaturePoint hmInfo) . surroundingChunks hmInfo $ pos
+closestFeaturePoint hmInfo pos = minimumBy (compare `on` distanceSquared pos) surroundingFeaturePoints
+    where surroundingFeaturePoints = map (generateFeaturePoint hmInfo) . surroundingChunks hmInfo $ pos
+
+-- general functions
+
+calculateNoiseValue :: HeightMapInfo -> Position -> Position -> Double
+calculateNoiseValue (HeightMapInfo _ chunkSize) first second = fromIntegral (distanceSquared first second) / fromIntegral (chunkSize ^ 2)
 
 surroundingChunks :: HeightMapInfo -> Position -> [Position]
-surroundingChunks hmInfo pos = surroundings
+surroundingChunks hmInfo pos = [(currChunkX + xOff, currChunkY + yOff) | xOff <- [-1..1], yOff <- [-1..1]]
     where (currChunkX, currChunkY) = chunkPosition hmInfo pos
-          surroundings = [(currChunkX + xOff, currChunkY + yOff) | xOff <- [-1..1], yOff <- [-1..1]]
 
 chunkPosition :: HeightMapInfo -> Position -> Position
 chunkPosition (HeightMapInfo _ chunkSize) (xPos, yPos) = (xPos `div` chunkSize, yPos `div` chunkSize)
 
 distanceSquared :: Position -> Position -> Int
-distanceSquared (firstX, firstY) (secondX, secondY) = deltaX ^ 2 + deltaY ^ 2
-    where deltaX = fromIntegral $ firstX - secondX
-          deltaY = fromIntegral $  firstY - secondY
+distanceSquared (firstX, firstY) (secondX, secondY) = deltaX * deltaX + deltaY * deltaY
+    where deltaX = firstX - secondX
+          deltaY = firstY - secondY
 
 generateFeaturePoint :: HeightMapInfo -> Position -> Position
 generateFeaturePoint (HeightMapInfo seed chunkSize) chunkPos@(chunkX, chunkY) = (xPos, yPos) 
     where chunkHash = hash2D seed chunkPos
-          deltaX = fromIntegral (chunkHash .&. 0xffff) `mod` chunkSize
-          deltaY = fromIntegral ((chunkHash `shiftR` 16) .&. 0xffff) `mod` chunkSize
-          xPos = fromIntegral $ chunkX * chunkSize + deltaX
-          yPos = fromIntegral $ chunkY * chunkSize + deltaY
+          deltaX = (chunkHash .&. 0xffff) `mod` chunkSize
+          deltaY = ((chunkHash `shiftR` 16) .&. 0xffff) `mod` chunkSize
+          xPos = chunkX * chunkSize + deltaX
+          yPos = chunkY * chunkSize + deltaY
 
 hash2D :: Seed -> Position -> Int
 hash2D seed (xPos, yPos) = hash
-    where posNum = fromIntegral xPos .|. (fromIntegral yPos :: Int) `shiftL` 16
+    where posNum = xPos .|. yPos `shiftL` 16
           toHash = seed `xor` posNum
           intermediate = (toHash `xor` 61) `xor` (toHash `shiftR` 16) 
           intermediate' = intermediate + (intermediate `shiftL` 3)
           intermediate'' = intermediate' `xor` (intermediate' `shiftR` 4)
           intermediate''' = intermediate'' * 0x27d4eb2d
           hash = intermediate''' `xor` (intermediate''' `shiftR` 15)
+
+index2D :: Size -> Vec.Vector a -> Position -> a
+index2D (width, _) toIndex (xPos, yPos) = toIndex ! (yPos * width + xPos)
+
+shiftPosition :: (Int, Int) -> Position -> Position
+shiftPosition (deltaX, deltaY) (posX, posY) = (posX + deltaX, posY + deltaY)
